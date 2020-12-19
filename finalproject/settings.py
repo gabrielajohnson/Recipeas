21770dbefcbe13813fdafa70d00d1088a29b8408
gabrielajohnson <gabrielagj23@gmail.com>
2020-12-18 20:07:54 -0500

    update files

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = '==ck^*))b@#r+en+^*0y6nt85o!$mcd!efd57zo)k#^wfvx7hp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -119,6 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 #MEDIA_ROOT = '/images/'
 MEDIA_URL = '/images/'
