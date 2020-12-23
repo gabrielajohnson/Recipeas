78afef604f4b58578e80bd981119e91eef0c35d2
gabrielajohnson <gabrielagj23@gmail.com>
2020-12-22 19:29:26 -0500

    remove dhango_heroku module

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
+#import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -130,4 +130,4 @@ STATIC_URL = '/static/'
 #MEDIA_ROOT = '/images/'
 MEDIA_URL = '/images/'
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
